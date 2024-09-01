e4cf3762839d1438fc9010011806c86a7e828501
kgpian-alok <106513417+kgpian-alok@users.noreply.github.com>
2024-09-01 23:57:11 +0530

    Create sample.cpp

---
+++ folder/folder2/sample.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define pp pop_back
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vll>
+using namespace std;
+void solve()
+{
+    int p=3;
+  cout<<p;
+}
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+	int t=4;
+	// cin >> t;
+	while(t--)
+	{
+	    solve();
+	    cout << endl;
+	}
+	return 0;
+}
